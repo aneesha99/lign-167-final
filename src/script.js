0f06b189b638c00be4d3a2be21dce0e90afd4ac7
aneesha <anramasw@ucsd.edu>
2022-11-22 00:24:58 -0800

    correct model loading

--- src/script.js
+++ src/script.js
@@ -4,22 +4,27 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//const camera = new THREE.PerspectiveCamera( 180, window.innerWidth / window.innerHeight, 0.1, 10000 );
+
 
 const renderer = new THREE.WebGLRenderer({
 	canvas: document.querySelector('canvas.webgl')
 });
 renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
-//document.body.appendChild( renderer.domElement );
 
 const loader = new GLTFLoader
 
-loader.load( 'room.glb', function ( gltf ) {
+loader.load( 'room.gltf', function ( gltf ) {
 	scene.add( gltf.scene );
 }, undefined, function ( error ) {
 	console.error( error );
 } );
 
-camera.position.z = 5;
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
+camera.position.z = 20;
+
 
 function animate() {
 	requestAnimationFrame( animate );
